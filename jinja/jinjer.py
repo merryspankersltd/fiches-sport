a4803d313394c5148e819b2c8be02e899d43f3e0
Merry Spankers ltd <marc.lauffer@gmail.com>
2021-09-23 03:12:31 +0200

    commit

--- jinja/jinjer.py
+++ jinja/jinjer.py
@@ -6,6 +6,7 @@ from jinja2 import Environment, FileSystemLoader
 from selenium import webdriver
 import json
 import base64
+import time
 
 def my_finalize(thing):
     return thing if thing is not None else 'NC'
@@ -32,7 +33,7 @@ chrome_options.add_experimental_option('prefs', prefs)
 chrome_options.add_argument('--kiosk-printing')
 chrome_options.add_argument("--headless")
 driver = webdriver.Chrome(options=chrome_options)
-driver.implicitly_wait(50)
+driver.implicitly_wait(5000)
 
 # get views
 with open(r'C:\Users\marcl\Documents\pro\fiches_sport_github\fiches-sport\fiches_json_rhone_202109230112.txt', 'r', encoding='utf-8') as f:
@@ -54,9 +55,9 @@ for view in json_views:
 
     # export pdf
     driver.get(html_path)
+    time.sleep(10)
     # driver.execute_script('window.print();')
     pdf = driver.execute_cdp_cmd("Page.printToPDF", {"printBackground": True})
-    driver.implicitly_wait(50)
     with open(pdf_path, "wb") as f:
         f.write(base64.b64decode(pdf['data']))
         
